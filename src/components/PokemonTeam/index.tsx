95bdb9adb3a8656ae32224873681768dccd69f47
Edjair Aguiar <edjair.aguiar@descomplica.com.br>
2021-09-09 16:05:45 -0300

    Alteração temporária na renderização estática da lista de Pokémons

--- src/components/PokemonTeam/index.tsx
+++ src/components/PokemonTeam/index.tsx
@@ -2,6 +2,17 @@ import * as S from './styles'
 import Avatar from 'components/Avatar'
 
 function PokemonTeam({ favPokemonSprite }: { favPokemonSprite: any }) {
+  const favPokemonStaticList = ['']
+  /* ALTERAÇÃO TEMPORÁRIA: 
+  O vetor vazio 'favPokemonStaticList' serve 
+  somente para renderizar a lista estaticamente no local host.
+  A lista real é renderizada através de props passadas pelo Storybook.
+  */
+
+  if (!favPokemonSprite) {
+    favPokemonSprite = favPokemonStaticList
+  }
+
   return (
     <S.Wrapper>
       <S.TeamList>
